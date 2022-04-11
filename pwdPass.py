8e4c9641933a892688ba7e6d0b521d1fc19484b0
Acceded <97315905+Acceded@users.noreply.github.com>
2022-04-11 18:42:34 +0200

    Add files via upload

---
+++ pwdPass.py
@@ -0,0 +1,32 @@
+import random
+
+
+LowPass = random.randint(1, 50)
+choosepass = input("Password length [1,50]: ")
+
+
+
+if choosepass > '50':
+    print("The password is too long")
+    print("Place a valid from 1 to 50")
+    print("Try again")
+    
+
+if choosepass < '1':
+    print("The password is too short")
+    print("Place a valid from 1 to 50")
+    print("Try again")
+
+    
+lower = "abcdefghijklmnopqrstuvwyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWYZ"
+NUMBER = "0123456789"
+Symbol = "@#€*.$^!%&"
+all=lower + upper + NUMBER +Symbol
+length = int(choosepass)
+password = "".join(random.sample(all,length))
+print(" ThePassword You Generated is :",password)
+import pyperclip
+pyperclip.copy('The text to be copied to the clipboard.')
+spam = pyperclip.copy(password)
+print("You have the password on your clipboard")
